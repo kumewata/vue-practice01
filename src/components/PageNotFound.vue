495dfa727fa92416ad7b404ef9f32653179de6d1
Wataru Kume <>
2018-09-08 18:12:04 +0900

    change route from helloworld to top, and add page not founde

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <main>
+      <h1> ページが見つかりません </h1>
+    </main>
+  </div>
+</template>
+
+<script>
+    export default {
+      title: 'Not Found',
+      description: ''
+      // name: "PageNotFound"
+    }
+</script>
+
+<style scoped>
+
+</style>
